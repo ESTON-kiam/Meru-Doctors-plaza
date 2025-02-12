92b8265aaf0a9ce26d301d38d78033f01f5f350a
ESTON-kiam <engestonbrandonkiama@gmail.com>
2025-02-12 11:15:26 +0300

    update

--- Database/Meru .sql
+++ Database/Meru .sql
@@ -20,11 +20,6 @@ CREATE TABLE `appointment` (
   `is_read` tinyint(1) DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- --------------------------------------------------------
-
---
--- Table structure for table `business_hours`
---
 
 CREATE TABLE `business_hours` (
   `id` int(11) NOT NULL,
@@ -34,18 +29,12 @@ CREATE TABLE `business_hours` (
   `open_hours` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `business_hours`
---
+
 
 INSERT INTO `business_hours` (`id`, `business_name`, `address`, `phone_number`, `open_hours`) VALUES
 (1, 'Meru Doctors Plaza', '2MV3+WPF, Municipality, Imenti North, Meru', '0705 656265', 'Open 24 hours');
 
--- --------------------------------------------------------
 
---
--- Table structure for table `contact`
---
 
 CREATE TABLE `contact` (
   `id` int(11) NOT NULL,
@@ -57,11 +46,7 @@ CREATE TABLE `contact` (
   `is_read` tinyint(1) DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- --------------------------------------------------------
 
---
--- Table structure for table `members`
---
 
 CREATE TABLE `members` (
   `id` int(11) NOT NULL,
@@ -74,18 +59,12 @@ CREATE TABLE `members` (
   `token_expiry` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `members`
---
+
 
 INSERT INTO `members` (`id`, `email`, `national_id`, `password`, `created_at`, `profile_picture`, `reset_token`, `token_expiry`) VALUES
 (1, 'merudocadmin@gmail.com', '40553254', '$2y$10$CIUCUh9HoRMMR8922yrpn.bfbU4S3WEKGpe/41xvNAehDxlaZHdwC', '2024-10-01 13:38:32', 'uploads/profile_pictures/6703e89056de3_11922.jpg', NULL, NULL),
 
--- --------------------------------------------------------
 
---
--- Table structure for table `orders`
---
 
 CREATE TABLE `orders` (
   `id` int(11) NOT NULL,
@@ -97,11 +76,7 @@ CREATE TABLE `orders` (
   `is_read` tinyint(1) DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- --------------------------------------------------------
 
---
--- Table structure for table `password_reset_tokens`
---
 
 CREATE TABLE `password_reset_tokens` (
   `email` varchar(255) NOT NULL,
@@ -109,11 +84,7 @@ CREATE TABLE `password_reset_tokens` (
   `created_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- --------------------------------------------------------
 
---
--- Table structure for table `sessions`
---
 
 CREATE TABLE `sessions` (
   `id` varchar(255) NOT NULL,
@@ -124,20 +95,14 @@ CREATE TABLE `sessions` (
   `last_activity` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
---
--- Dumping data for table `sessions`
---
+
 
 INSERT INTO `sessions` (`id`, `user_id`, `ip_address`, `user_agent`, `payload`, `last_activity`) VALUES
 ('nNIFxvOyufFgRD5xCDBoLCivs7c5TO4xGCZZzOdF', NULL, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36 Edg/130.0.0.0', 'YTozOntzOjY6Il90b2tlbiI7czo0MDoibmhpVE1GblJQUTZaOUlURVFwVDl1bTZkdHViZ2lMVDRnSTR3WXZ1TyI7czo5OiJfcHJldmlvdXMiO2E6MTp7czozOiJ1cmwiO3M6MjE6Imh0dHA6Ly8xMjcuMC4wLjE6ODAwMCI7fXM6NjoiX2ZsYXNoIjthOjI6e3M6Mzoib2xkIjthOjA6e31zOjM6Im5ldyI7YTowOnt9fX0=', 1729845199),
 ('RIfCbGdLxHY6fsWoLHYGA0h20SVfXfRlIHtWNdM3', NULL, '::1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36 Edg/130.0.0.0', 'YTozOntzOjY6Il90b2tlbiI7czo0MDoiaVRvQ011NVRSOTNHaGdNTW1qY1FJYXV2dllJdU42R3Jjd3loc2JpdCI7czo5OiJfcHJldmlvdXMiO2E6MTp7czozOiJ1cmwiO3M6NTA6Imh0dHA6Ly9sb2NhbGhvc3QvZGFzaGJvYXJkL21lcnVkb2N0b3JzcGxhemEvcHVibGljIjt9czo2OiJfZmxhc2giO2E6Mjp7czozOiJvbGQiO2E6MDp7fXM6MzoibmV3IjthOjA6e319fQ==', 1729844397),
 ('TnqBAhQvfnmyiL6tK1WFJBFYRuEzZBmgCcyt99jI', NULL, '::1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36 Edg/130.0.0.0', 'YTozOntzOjY6Il90b2tlbiI7czo0MDoicjV6cFBKeFVUMGNZaVZrWGxOV0tWeHU0SVhQUXIxejRibjYzUUVSWSI7czo5OiJfcHJldmlvdXMiO2E6MTp7czozOiJ1cmwiO3M6NTA6Imh0dHA6Ly9sb2NhbGhvc3QvZGFzaGJvYXJkL21lcnVkb2N0b3JzcGxhemEvcHVibGljIjt9czo2OiJfZmxhc2giO2E6Mjp7czozOiJvbGQiO2E6MDp7fXM6MzoibmV3IjthOjA6e319fQ==', 1729841067);
 
--- --------------------------------------------------------
 
---
--- Table structure for table `subscribers`
---
 
 CREATE TABLE `subscribers` (
   `id` int(11) NOT NULL,
@@ -145,110 +110,67 @@ CREATE TABLE `subscribers` (
   `is_read` tinyint(1) DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `subscribers`
---
+
 
 INSERT INTO `subscribers` (`id`, `email`, `is_read`) VALUES
 
 
---
--- Indexes for dumped tables
---
 
---
--- Indexes for table `appointment`
---
 ALTER TABLE `appointment`
   ADD PRIMARY KEY (`id`);
 
---
--- Indexes for table `business_hours`
---
+
 ALTER TABLE `business_hours`
   ADD PRIMARY KEY (`id`);
 
---
--- Indexes for table `contact`
---
+
 ALTER TABLE `contact`
   ADD PRIMARY KEY (`id`);
 
---
--- Indexes for table `members`
---
+
 ALTER TABLE `members`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `email` (`email`);
 
---
--- Indexes for table `orders`
---
+
 ALTER TABLE `orders`
   ADD PRIMARY KEY (`id`);
 
---
--- Indexes for table `password_reset_tokens`
---
+
 ALTER TABLE `password_reset_tokens`
   ADD PRIMARY KEY (`email`);
 
---
--- Indexes for table `sessions`
---
+
 ALTER TABLE `sessions`
   ADD PRIMARY KEY (`id`),
   ADD KEY `sessions_user_id_index` (`user_id`),
   ADD KEY `sessions_last_activity_index` (`last_activity`);
 
---
--- Indexes for table `subscribers`
---
+
 ALTER TABLE `subscribers`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `email` (`email`);
 
---
--- AUTO_INCREMENT for dumped tables
---
 
---
--- AUTO_INCREMENT for table `appointment`
---
 ALTER TABLE `appointment`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 
---
--- AUTO_INCREMENT for table `business_hours`
---
 ALTER TABLE `business_hours`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
---
--- AUTO_INCREMENT for table `contact`
---
+
 ALTER TABLE `contact`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
 
---
--- AUTO_INCREMENT for table `members`
---
+
 ALTER TABLE `members`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
---
--- AUTO_INCREMENT for table `orders`
---
+
 ALTER TABLE `orders`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 
---
--- AUTO_INCREMENT for table `subscribers`
---
+
 ALTER TABLE `subscribers`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
